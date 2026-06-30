e41833adf2e6f25b9351d6499629dd1ddd79e974
leprogrammeur <p0846714@gmail.com>
2026-06-30 19:15:40 +0000

    premier commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+$categories = [
+
+   0 =>      [
+            "code" => "coo0",
+            "nom" => "categorie0",
+            "produits" => [
+                  0 => [
+                    "nom" => "produit1",
+                    "reference" => "ref1",
+                    "prix" => 3000,
+                    "quantite" => 5 
+                  ],
+                  1 => [
+                    "nom" => "produit2",
+                    "reference" => "ref2",
+                    "prix" => 2000,
+                    "quantite" => 3 
+                  ]
+            ]
+         ],
+   1 =>      [
+            "code" => "coo1",
+            "nom" => "categorie1",
+            "produits" => []
+         ]
+];
+
+
+ function afficheCategorieSansProduit(array $categories): void{
+    foreach ($categories as  $categorie ) {
+        if (empty($categorie["produits"])) {
+            echo $categorie["nom"]."\n";
+        }
+    }
+ }
+ afficheCategorieSansProduit($categories);
+
+
+
+?>
