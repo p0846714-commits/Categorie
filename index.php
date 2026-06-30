63e93b39b95be2a5a16ca965df6c21d81b454e8f
leprogrammeur <p0846714@gmail.com>
2026-06-30 22:03:33 +0000

    recherche de categorie par cles

--- index.php
+++ index.php
@@ -51,5 +51,29 @@ $categories = [
     }
         return true;
  }
+
+
+
+ function rechercheCategorieParCle(array $categories, string $key, string $value): int|bool {
+    foreach ($categories as $index  => $categorie ) {
+        if (($categorie[$key]) === $value) {
+            return $index ;
+        }
+    } 
+    return false;
+ }
+
+ function saisieChampObligatoireEtUnique(array $categories,string $smsSaisie, string $smsError,string $key): string{
+        
+    $valueIsValid = true;
+    do {   
+        $value = saisieChaine($smsSaisie);
+        $valueIsValid = champObligatoire($value,$smsError);
+        if($valueIsValid){     
+            $valueIsValid =rechercheCategorieParCle($categories,$key,$value);
+        }
+    } while (!$valueIsValid);
+    return $value;
+ }
  
 ?>
